2559d2cc419da3acd37217453db985e112fe8cf1
NewToProgramming58 <NewToProgramming58@users.noreply.github.com>
2022-11-15 10:14:18 +0200

    Login, register functionality with textfile

--- PRG2x2_Project/PRG2x2_Project/Login.cs
+++ PRG2x2_Project/PRG2x2_Project/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PRG2x2_Project.User_Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace PRG2x2_Project
 {
     public partial class Login : Form
     {
+        List<User> users = new List<User>();
         public Login()
         {
             InitializeComponent();
@@ -19,10 +21,51 @@ namespace PRG2x2_Project
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            Details frm = new Details();
-            frm.Show();
-            frm.StoreLoginForm(this);
-            this.Hide();
+            string username = txbUsername.Text;
+            string password = txbPassword.Text;
+
+            if (username.Length != 0 && password.Length != 0) 
+            {                
+                User user = users.FirstOrDefault(x => x.Username == username);
+                if (user != null)
+                {
+                    
+                    if (password.Equals(user.Password))
+                    {
+                        MessageBox.Show("Login successfull");
+                        Details frm = new Details();
+                        this.Hide();
+                        frm.Show();
+                        frm.StoreLoginForm(this);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Password is incorrect");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Username does not exist, Please register first");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill in a usename and password");
+            }
+        }
+
+        private void Login_Activated(object sender, EventArgs e)
+        {
+            FileHandler fileHandler = new FileHandler();
+            users = fileHandler.GetUsers();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            FileHandler fileHandler = new FileHandler();
+            User user = new User(txbUsername.Text, txbPassword.Text);
+            fileHandler.addUser(user);
+            MessageBox.Show($"Registered {user.Username}, Welcome");
         }
     }
 }
---
+++ PRG2x2_Project/PRG2x2_Project/User Classes/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2x2_Project.User_Classes
+{
+    internal class User
+    {
+        string username;
+        string password;
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+
+        override
+        public string ToString()
+        {
+            return $"{Username},{Password}";
+        }
+    }
+}
